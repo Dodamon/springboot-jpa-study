3eb6272b0ff8b9fe64db6ecb39b23028d479c1b3
Dodamond <dodamond222@gmail.com>
2023-01-15 13:20:24 +0900

    [23-01-15] 프로젝트환경설정

---
+++ 1/jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello:)");
+        return "hello";
+    }
+}
---
+++ 1/jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
